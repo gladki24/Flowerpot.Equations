4721f9b615914f50d989c67dcea7800037930d62
Seweryn Gładysz <seweryngla@hotmail.com>
2020-11-02 23:56:08 +0100

    create simple view

--- Flowerpot.Equations.View/MainWindow.xaml.cs
+++ Flowerpot.Equations.View/MainWindow.xaml.cs
@@ -12,17 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Flowerpot.Equations.Domain;
+using Flowerpot.Equations.Domain.UnitTests;
 
 namespace Flowerpot.Equations.View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // TODO Logic Layer (services?)
+        // TODO Create NumberBox?
+        // TODO Create ViewModels?
+        private void GenerateQuadraticFormButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var factorA = QuadraticFactorATextBox.Text;
+            var factorB = QuadraticFactorBTextBox.Text;
+            var factorC = QuadraticFactorCTextBox.Text;
+
+            var equation = new QuadraticEquation(Double.Parse(factorA), Double.Parse(factorB), Double.Parse(factorC));
+
+            QuadraticResult.Text = equation.ToString();
+        }
+
+        private void GenerateLinearFormButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var factorA = LinearFactorATextBox.Text;
+            var factorB = LinearFactorBTextBox.Text;
+
+            var equation = new LinearEquation(Double.Parse(factorA), Double.Parse(factorB));
+
+            LinearResult.Text = equation.ToString();
         }
     }
 }
